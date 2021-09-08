2dc7ceb3d32a44dfecee66076775b705a4ff0ef8
Andrea Coronese <sixpounder@protonmail.com>
2021-09-09 01:28:37 +0200

    refactor folder structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-import Header from "./components/Header.vue";
-import Layout from "./components/Layout.vue";
-import Footer from "./components/Footer.vue";
-import Prologue from "./components/Prologue.vue";
-import WorkHistory from "./components/WorkHistory.vue";
-import Education from "./components/Education.vue";
-import Contacts from "./components/Contacts.vue";
-import Skills from "./components/Skills.vue";
-import SpokenLanguages from "./components/Languages.vue";
-import SoftSkills from "./components/SoftSkills.vue";
+import Header from "./components/resume/Header.vue";
+import Layout from "./components/resume/Layout.vue";
+import Footer from "./components/resume/Footer.vue";
+import Prologue from "./components/resume/Prologue.vue";
+import WorkHistory from "./components/resume/WorkHistory.vue";
+import Education from "./components/resume/Education.vue";
+import Contacts from "./components/resume/Contacts.vue";
+import Skills from "./components/resume/Skills.vue";
+import SpokenLanguages from "./components/resume/Languages.vue";
+import SoftSkills from "./components/resume/SoftSkills.vue";
 
 export default defineComponent({
   components: {
