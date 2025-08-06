4b65d277f4dba1c78b80aa03a65547bdc4cf620b
Andrea Coronese <sixpounder@protonmail.com>
2025-08-06 11:47:39 +0200

    feat: update skills

--- src/components/resume/Skills.vue
+++ src/components/resume/Skills.vue
@@ -5,10 +5,15 @@
       <Skill :rating="5">Javascript / Typescript Specialist</Skill>
       <Skill :rating="5">API Design Specialist</Skill>
       <Skill :rating="5">UI/UX Design</Skill>
-      <Skill :rating="5">Modern Front End Web Development</Skill>
+      <Skill :rating="5">Angular</Skill>
+      <Skill :rating="5">React</Skill>
+      <Skill :rating="5">Vue</Skill>
       <Skill :rating="5">NodeJS Expert</Skill>
       <Skill :rating="5">Web design</Skill>
-      <Skill :rating="4">Rust</Skill>
+      <Skill :rating="5">Java</Skill>
+      <Skill :rating="5">Spring Boot</Skill>
+      <Skill :rating="5">Kafka Streams / Connect</Skill>
+      <Skill :rating="5">Rust</Skill>
       <Skill :rating="4">Ruby</Skill>
       <Skill :rating="3">C/C++</Skill>
     </div>
